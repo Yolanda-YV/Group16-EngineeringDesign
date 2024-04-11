99669d5607732f6993299982e19ecc07eabb9ca7
Yolanda-YV <yolanda.n.vega@gmail.com>
2024-04-11 18:30:55 -0400

    installed axios and monaco editor, saved them to package.json for easy installation

--- AI-Python-Tutor/aipt-app/src/App.jsx
+++ AI-Python-Tutor/aipt-app/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Using useRef to hold code editor value to store it between re-renders 
+  // - doesn't reset on every render 
+  // - doesn't trigger a re-render on change)
+  const codeValueRef = useRef(null)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='whole-page'>
+
+    </div>
   )
 }
 
