9179c4bf96b432400117f73fa0fec67437c4d99d
Yolanda-YV <yolanda.n.vega@gmail.com>
2024-05-18 15:22:04 -0400

    Added Logo, new name, and styling

--- AI-Python-Tutor/aipt-app/src/components/CodeTool.jsx
+++ AI-Python-Tutor/aipt-app/src/components/CodeTool.jsx
@@ -1,17 +1,18 @@
 import react from 'react';
 import Editor from '@monaco-editor/react';
 import '../index.css';
-
+// NOTE: Editor height is 100vh - 90px (Navbar) - 50px (Submit button) - 5rem (Gaps and Padding)
 const CodeTool = ({handleEditorChange, handleSubmit}) => {
     return (
         <form className='code-tool' onSubmit={handleSubmit}>
             <Editor 
-                height='85.3vh' 
+                height='calc(100vh - 100px - 50px - 5rem)'
                 width='100%'
                 theme='vs-dark'
                 defaultLanguage='python'
                 defaultValue='# Write your code here'
                 onChange={handleEditorChange}></Editor>
+                <div style={{height: 'stretch'}}></div>
             <button className='submit-btn' type='submit'>Submit</button>
         </form>
     );
