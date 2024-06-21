fc24eed74af3f47a92f23701026ca5c6e009c586
Yolanda-YV <yolanda.n.vega@gmail.com>
2024-06-21 16:27:56 -0400

    Cleaned code

--- AI-Python-Tutor/aipt-app/src/pages/Dashboard.jsx
+++ AI-Python-Tutor/aipt-app/src/pages/Dashboard.jsx
@@ -10,9 +10,6 @@ const Dashboard = () => {
     const [topics, setTopics] = useState(null);
     const [topicCalculations, setTopicCalculations] = useState(null)
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [tasks, setTasks] = useState(null);
-    const [topicTasksData, setTopicTasksData] = useState([]);
 
     useEffect(() => {
       const fetchUserData = async () => {
@@ -52,38 +49,6 @@ const Dashboard = () => {
           
           setLoading(false);
 
-
-        //   // fetch topic and task data
-        //   const { data: topicTasksData, error: topicTasksError } = await supabase
-        //    .from('Tasks')
-        //    .select('content, topic_id, Topics (name, level_id)')
-        //    .order('id');
-
-        //   if (topicTasksError) {
-        //     throw topicTasksError;
-        //   }
- 
-        //  // Set topic tasks data
-        //  setTopicTasksData(topicTasksData);
-
-        //  console.log('Topic Tasks data:', topicTasksData);
-
-        //  // fetch user tasks data
-        //  const { data: tasksData, error: tasksError } = await supabase
-        //    .from('UserTasks')
-        //    .select('status, started_at, feedback, completed_at, score')
-        //    .eq('user_id', user.id);
-
-        //   if (tasksError) {
-        //   throw tasksError;
-        //   }
-
-        //   // Set tasks data
-        //  setTasks(tasksData);
-
-        //  console.log('Tasks data:', tasksData);
-
-        //  setLoading(false);
         } catch (error) {
           setError(error.message);
         }
@@ -116,55 +81,6 @@ const Dashboard = () => {
         
       </div>
     );
-
-    // return (
-    //   <div className='dashboard-page'>
-    //     <h1>Welcome to the Dashboard</h1>
-    //     <div className='user-info-card'>
-    //       <h2>User Info</h2>
-    //       <p><strong>Email:</strong> {userInfo ? userInfo.email : null}</p>
-    //       <p><strong>Name:</strong> {userInfo ? userInfo.username : null}</p>
-    //       <p><strong>Level:</strong> {userInfo ? userInfo.level : null}</p>
-    //     </div>
-    //     <div className='topics-tasks-container'>
-    //     <div className='topics-card'>
-    //     <h2>Topics</h2>
-    //     {topicTasksData.length > 0 ? (
-    //       <ul>
-    //         {topicTasksData.map((topics, index) => (
-    //           <li key={index}>
-    //             <p><strong>Name:</strong> {topics.Topics.name}</p>
-    //             <p><strong>Content:</strong> {topics.content}</p>
-    //             <p><strong>Level:</strong> {topics.Topics.level_id}</p>
-    //             <p><strong>Topic #:</strong> {topics.topic_id}</p>
-    //           </li>
-    //         ))}
-    //       </ul>
-    //     ) : (
-    //       <p>No topics found.</p>
-    //     )}
-    //   </div>
-    //   <div className='tasks-card'>
-    //     <h2>Tasks</h2>
-    //     {tasks.length > 0 ? (
-    //       <ul>
-    //         {tasks.map((task, index) => (
-    //           <li key={index}>
-    //             <p><strong>Status:</strong> {task.status}</p> 
-    //             <p><strong>Started At:</strong> {task.started_at}</p>
-    //             <p><strong>Feedback:</strong> {task.feedback}</p>
-    //             <p><strong>Completed At:</strong> {task.completed_at}</p>
-    //             <p><strong>Score:</strong> {task.score}</p>
-    //           </li>
-    //         ))}
-    //       </ul>
-    //     ) : (
-    //       <p>No tasks found for this user.</p>
-    //     )}
-    //     </div>
-    //   </div>
-    //   </div>
-    // );
   };
   
   export default Dashboard;
