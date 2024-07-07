12099a7458ac23593820759765a5d875000429e3
carl-sff <crlvillarosa@gmail.com>
2024-07-07 17:01:53 -0400

    validate code incomplete

--- AI-Python-Tutor/aipt-app/src/utilities/Supabase.js
+++ AI-Python-Tutor/aipt-app/src/utilities/Supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-let supabaseUrl = "https://tbycnfeusflvnfhykvph.supabase.co";
-let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRieWNuZmV1c2Zsdm5maHlrdnBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY3NzI4NzcsImV4cCI6MjAzMjM0ODg3N30.uPBhXBrcp2_p-DJ-5jNmABAK1As09OpWIs7qKyf5Mv4";
+const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
