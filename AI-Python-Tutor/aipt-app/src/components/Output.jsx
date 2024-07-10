1a7d2ef14d0d9583abb0bf1ddda7cd193d781aac
arianaxgalindo <agalindo2021@fau.edu>
2024-07-09 23:39:24 -0400

    Update Output.jsx

--- AI-Python-Tutor/aipt-app/src/components/Output.jsx
+++ AI-Python-Tutor/aipt-app/src/components/Output.jsx
@@ -1,21 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import '../index.css';
 
-const Output = ({output, task, getTask, feedback, hint, isCorrect}) => {
-    // State to control whether to show the hint popup
-    const [showHint, setShowHint] = useState(false);
-
-    useEffect(() => {
-        // Update showHint whenever isCorrect changes to false
-        if (isCorrect === false) {
-            setShowHint(true);
-        }
-    }, [isCorrect]);
-
-    // Function to close the hint popup
-    const closeHint = () => {
-        setShowHint(false);
-    };
+const Output = ({output, task, getTask, feedback, isCorrect}) => {
   
     return (
         <div className='output'>
@@ -27,25 +13,18 @@ const Output = ({output, task, getTask, feedback, hint, isCorrect}) => {
               <h1>Output</h1>
               <pre>{output}</pre>
             </div>
+            {/* feedback shows when isCorrect is true */}
+            {isCorrect === true && (
             <div className='feedback'>
               <h1>Feedback</h1>
               <p>{feedback}</p>
             </div>
+            )}
             <div className='isCorrect'>
               <h1>Correct</h1>
               <p>{isCorrect !== null ? (isCorrect ? 'Yes' : 'No') : 'Not checked yet'}</p>
             </div>
           </div>
-          {/* Display hint popup if showHint is true */}
-          {showHint && (
-                <div className='hint-popup'>
-                    <div className='hint-content'>
-                        <button className='close-hint' onClick={closeHint}>X</button>
-                        <h1>Hint</h1>
-                        <p>{hint}</p>
-                    </div>
-                </div>
-            )}
         </div>
       );
     };
